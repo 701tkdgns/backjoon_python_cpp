d7e5b38cafd99a7f7464efa3a4e613b417c52fb9
psh <701tkdgns@inu.ac.kr>
2022-07-12 22:51:01 +0900

    0712

--- 2022/7/0711/Q17127.py
+++ 2022/7/0711/Q17127.py
@@ -1,17 +1,19 @@
-def Mul(start, end):
+def MUL(start, end):
     ret = lst[start]
-    for i in range(start + 1, end + 1):
+    for i in range(start+1, end):
         ret *= lst[i]
     return ret
 
 
-N, SUM, ANS = int(input()), 0, 0
+N = int(input())
+res = 0
 lst = list(map(int, input().split()))
+s = 0
 for i in range(N - 3):
-    SUM = 0
+    s = 0
     for j in range(i + 1, N - 2):
         for k in range(j + 1, N - 1):
             for r in range(k + 1, N):
-                SUM = Mul(i, j - 1) + Mul(j, k - 1) + Mul(k, r - 1) + Mul(r, N)
-                ANS = max(SUM, ANS)
-print(ANS)
+                s = MUL(i, j) + MUL(j, k) + MUL(k, r) + MUL(r, N)
+                res = max(res, s)
+print(res)

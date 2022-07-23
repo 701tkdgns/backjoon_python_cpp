0e02dc5ba34ccf378c3932277f27cbd5b094fdba
psh <701tkdgns@inu.ac.kr>
2022-07-23 18:48:25 +0900

    0723

---
+++ 2022/7/0723/Q17070.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+def bfs():
+    visit[0][0] = True
+    visit[0][1] = True
+    cnt = 0
+    dq = deque([[0, 1]])
+    tmp = [[0, 0], [0, 1]]
+    direction_o = [(1, 0), (1, 1)]
+    direction_w = [(0, 1), (1, 1)]
+    direction_t = [(1, 0), (0, 1), (1, 1)]
+    while dq:
+        x, y = tmp[0]
+        i, j = tmp[1]
+        del tmp[0], tmp[1]
+        x, y = abs(x - i), abs(y - j)
+        if x == 1 and y == 0:
+            for dx, dy in direction_o:
+                pass
+        elif x == 0 and y == 1:
+            for dx, dy in direction_w:
+                pass
+        elif x == 1 and y == 1:
+            for dx, dy in direction_t:
+                pass
+
+N = int(input())
+lst = []
+visit = [[False] * N for _ in range(N)]
+for _ in range(N):
+    lst.append(list(map(int, input().split())))

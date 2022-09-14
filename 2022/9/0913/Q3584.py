f1b5532d8b5c2dfce2bb7e3bdf9b3a398bb11130
psh <701tkdgns@inu.ac.kr>
2022-09-14 22:14:21 +0900

    0914

--- 2022/9/0913/Q3584.py
+++ 2022/9/0913/Q3584.py
@@ -1,21 +1,27 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 6)
 
 
-def tree(n, idx):
-    if n == S or n == E:
-        return idx
-    pass
-
-
-T = int(input())
-for _ in range(T):
+for _ in range(int(input())):
     N = int(input())
-    lst = [[] for _ in range(N + 1)]
-    dist = [-1 for _ in range(N + 1)]
+    # lst = [[] for _ in range(N + 1)]
+    # visit = [False for _ in range(N + 1)]
+    parent = [0] * (N + 1)
     for _ in range(N - 1):
         a, b = map(int, input().split())
-        lst[a].append(b)
-        lst[b].append(a)
-    S, E = map(int, input().split())
-    tree(1, 1)
+        parent[b] = a
+        # lst[a].append(b)
+        # lst[b].append(a)
+    s, e = map(int, input().split())
+    s_parent, e_parent = [0, s], [0, e]
+    while parent[s]:
+        s_parent.append(parent[s])
+        s = parent[s]
+    while parent[e]:
+        e_parent.append(parent[e])
+        e = parent[e]
+
+    idx = 1
+    while s_parent[-idx] == e_parent[-idx]:
+        idx += 1
+    print(s_parent[-idx + 1])

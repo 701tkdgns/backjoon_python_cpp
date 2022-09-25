3d4b28cf2414048297e47deef01eb3b83c854063
701tkdgns <701tkdgns@inu.ac.kr>
2022-09-25 15:58:53 +0900

    0925

---
+++ 2022/9/0925/Q20040.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def find(x):
+    if x == parent[x]:
+        return x
+    else:
+        y = find(parent[x])
+        parent[x] = y
+        return y
+
+def union(x, y, idx):
+    global res
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parent[max(x, y)] = parent[min(x, y)]
+    elif res == 0:
+        res = idx
+
+
+n, m = map(int, input().split())
+parent = [i for i in range(n)]
+res = 0
+for i in range(m):
+    a, b = map(int, input().split())
+    union(a, b, i + 1)
+print(res)

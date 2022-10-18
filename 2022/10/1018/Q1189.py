df86a7b3a35fd738af0296821b043645e72e0a9d
701tkdgns <701tkdgns@inu.ac.kr>
2022-10-19 00:17:17 +0900

    1018

--- 2022/10/1018/Q1189.py
+++ 2022/10/1018/Q1189.py
@@ -1,28 +1,29 @@
 import sys
 
-sys.setrecursionlimit(10 ** 5)
+sys.setrecursionlimit(10 ** 6)
 
 
-def dfs(x, y):
-    if x == R - 1 and y == C - 1 and visit[x][y] == K:
-        res.append([x, y])
+def dfs(x, y, cnt):
+    global res
+    if x == 0 and y == C - 1 and cnt == K:
+        res += 1
+        return
 
     for dx, dy in direction:
         nx, ny = x + dx, y + dy
-        if 0 <= nx < R and 0 <= ny < C and visit[nx][ny] == -1 and lst[nx][ny] == ".":
-            visit[nx][ny] = visit[x][y] + 1
-            dfs(nx, ny)
+        if 0 <= nx < R and 0 <= ny < C and not visit[nx][ny] and lst[nx][ny] != "T":
+            visit[nx][ny] = True
+            dfs(nx, ny, cnt + 1)
+            visit[nx][ny] = False
 
 
 R, C, K = map(int, input().split())
 lst = []
-res = []
+res = 0
 direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 for _ in range(R):
     lst.append(list(map(str, input().rstrip())))
-visit = [[-1 for _ in range(C)] for _ in range(R)]
-visit[0][0] = 0
-dfs(0, 0)
+visit = [[False for _ in range(C)] for _ in range(R)]
+visit[R-1][0] = True
+dfs(R-1, 0, 1)
 print(res)
-for i in visit:
-    print(*i)

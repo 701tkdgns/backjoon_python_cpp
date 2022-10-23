2aafbd835654979116a58aeacfa1cad55a59ca4d
701tkdgns <701tkdgns@inu.ac.kr>
2022-10-23 10:18:11 +0900

    1023

--- 2022/10/1021/Q14725.py
+++ 2022/10/1021/Q14725.py
@@ -1,5 +1,19 @@
 import sys
-sys.setrecursionlimit(10**6)
+
+sys.setrecursionlimit(10 ** 6)
+
+class Trie:
+    def __init__(self):
+        self.root = {}
+
+    def add(self, foods):
+        cur = self.root
+
+        for food in foods:
+            if food not in cur:
+                cur[food] = {}
+            cur = cur[food]
+        cur[0] = True
 
 
 N = int(input())
@@ -7,4 +21,3 @@ lst = []
 for _ in range(N):
     tmp = list(map(str, input().split()))
     lst.append(tmp[1:])
-print(lst)

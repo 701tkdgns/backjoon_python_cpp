f85e544bb0a6237e6d52cccd8d2993b6cc2ba5e8
701tkdgns <701tkdgns@inu.ac.kr>
2022-10-24 23:50:24 +0900

    1024

--- 2022/10/1021/Q14725.py
+++ 2022/10/1021/Q14725.py
@@ -1,10 +1,26 @@
 import sys
-sys.setrecursionlimit(10**6)
 
+sys.setrecursionlimit(10 ** 6)
 
 N = int(input())
 lst = []
 for _ in range(N):
     tmp = list(map(str, input().split()))
     lst.append(tmp[1:])
-print(lst)
+
+lst.sort()
+dash = '--'
+answer = []
+for i in range(N):
+    if i == 0:
+        for j in range(len(lst[i])):
+            answer.append(dash * j_lst[i][j])
+    else:
+        idx = 0
+        for j in range(len(lst[i])):
+            if lst[i-1][j] != lst[i][j] or len(lst[i-1]) <= j:
+                break
+            else:
+                idx = j + 1
+        for j in range(idx, len(lst[i])):
+            answer.append(dash * j + lst[i][j])

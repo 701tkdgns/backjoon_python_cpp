5518d95167986b576fa58e9a3ff5f33dae5abc6a
701tkdgns <701tkdgns@inu.ac.kr>
2022-10-30 20:07:12 +0900

    1030

--- 2022/10/1027/Q18405.py
+++ 2022/10/1027/Q18405.py
@@ -1,30 +1,30 @@
 from collections import deque
 
 def bfs():
-    global dq
-    new_dq = deque()
-    while dq:
-        x, y = dq.popleft()
+    global q
+    nq = []
+    while q:
+        v, x, y = q[0]
+        del q[0]
         for dx, dy in direction:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N and visit[nx][ny] == 0 and lst[nx][ny] == 0:
-                visit[nx][ny] = visit[x][y]
-                new_dq.append((nx, ny))
-    dq = new_dq.copy()
+            if 0 <= nx < N and 0 <= ny < N and lst[nx][ny] == 0:
+                nq.append([lst[x][y], nx, ny])
+                lst[nx][ny] = lst[x][y]
+    q = nq.copy()
 
 direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 N, K = map(int, input().split())
 lst = []
-visit = [[0 for _ in range(N)] for _ in range(N)]
-dq = deque()
-for x in range(N):
+q = []
+for i in range(N):
     tmp = list(map(int, input().split()))
-    for y in range(len(tmp)):
-        if tmp[y] != 0:
-            dq.append([x, y])
-            visit[x][y] = tmp[y]
+    for j in range(len(tmp)):
+        if tmp[j] != 0 and tmp[j] <= K:
+            q.append([tmp[j], i, j])
     lst.append(tmp)
 S, X, Y = map(int, input().split())
-for i in range(S):
+q.sort()
+for _ in range(S):
     bfs()
-print(visit[X-1][Y-1])
+print(lst[X-1][Y-1])

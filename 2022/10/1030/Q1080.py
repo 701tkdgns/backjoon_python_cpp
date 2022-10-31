daa9cd6662c210f85addc41fc21a605685857c1e
701tkdgns <701tkdgns@inu.ac.kr>
2022-10-31 23:40:02 +0900

    1031

--- 2022/10/1030/Q1080.py
+++ 2022/10/1030/Q1080.py
@@ -1,19 +1,33 @@
+def reverse(x, y):
+    for i in range(x, x + 3):
+        for j in range(y, y + 3):
+            init[i][j] = 1 - init[i][j]
+
+
+def check():
+    for i in range(N):
+        for j in range(M):
+            if init[i][j] != target[i][j]:
+                return False
+    return True
+
+
 N, M = map(int, input().split())
 init, target = [], []
+cnt = 0
 for _ in range(N):
     init.append(list(map(int, input().rstrip())))
 
 for _ in range(N):
     target.append(list(map(int, input().rstrip())))
 
+for i in range(N - 2):
+    for j in range(M - 2):
+        if init[i][j] != target[i][j]:
+            reverse(i, j)
+            cnt += 1
 
-init_0, init_1 = 0, 0
-target_0, target_1 = 0, 0
-
-for i in range(N):
-    init_0 += init[i].count(0)
-    init_1 += init[i].count(1)
-    target_0 += target[i].count(0)
-    target_1 += target[i].count(1)
-
-print(init_0, init_1, target_0, target_1)
+if check():
+    print(cnt)
+else:
+    print(-1)
---
+++ 2022/10/1031/Q1963.py
@@ -0,0 +1,7 @@
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
+    res = 0
+    while A != B:
+        pass
+    print(res)

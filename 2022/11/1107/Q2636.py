3524c50d8a3b0c25096d49e794117f29ed3bbe22
701tkdgns <701tkdgns@inu.ac.kr>
2022-11-08 23:40:59 +0900

    1108

--- 2022/11/1107/Q2636.py
+++ 2022/11/1107/Q2636.py
@@ -4,17 +4,17 @@ from collections import deque
 def bfs(x, y):
     dq = deque()
     dq.append([x, y])
+    visit[x][y] = 1
     while dq:
         x, y = dq.popleft()
         for dx, dy in direction:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < M and lst[x][y] == 1 and visit[nx][ny] == 0:
+            if 0 <= nx < N and 0 <= ny < M and lst[x][y] == 0 and visit[nx][ny] == 0:
                 if lst[nx][ny] == 1:
-                    dq.append([nx, ny])
                     visit[nx][ny] = 1
-                if lst[nx][ny] == 0:
+                    lst[nx][ny] = 0
+                else:
                     visit[nx][ny] = 1
-                    lst[x][y] = 0
 
 
 N, M = map(int, input().split())
@@ -26,7 +26,7 @@ visit = [[0 for _ in range(M)] for _ in range(N)]
 
 for i in range(N):
     for j in range(M):
-        if lst[i][j] == 1 and visit[i][j] == 0:
+        if lst[i][j] == 0 and visit[i][j] == 0:
             bfs(i, j)
 
 for i in range(N):

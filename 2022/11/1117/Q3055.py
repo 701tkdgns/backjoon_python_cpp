ba8a158f238c595ab29d158d08b4fdc2e9ee38dc
701tkdgns <701tkdgns@inu.ac.kr>
2022-11-18 22:52:14 +0900

    1118

--- 2022/11/1117/Q3055.py
+++ 2022/11/1117/Q3055.py
@@ -1,57 +1,59 @@
 from collections import deque
 
-
-def water():
-    for _ in range(len(wat)):
-        x, y = wat.popleft()
-        for dx, dy in direction:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < R and 0 <= ny < C:
-                if not lst[nx][ny] == "X":
-                    lst[nx][ny] = "*"
-                    wat.append([nx, ny])
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 
-def bfs(init_x, init_y):
-    bibi = deque()
-    bibi.append([init_x, init_y])
-    visit[init_x][init_y] = 0
-    while bibi:
-        x, y = bibi.popleft()
-        if x == e[0] and y == e[1]:
-            return True
-        for dx, dy in direction:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < R and 0 <= ny < C and not visit[nx][ny] and (lst[nx][ny] == "." or lst[nx][ny] == "S"):
-                visit[nx][ny] = visit[x][y] + 1
-                bibi.append([nx, ny])
-
+def bfs(x, y):
+    q.append([x, y])
+    c[x][y] = 1
+    while q:
+        qlen = len(q)
+        while qlen:
+            x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < m:
+                    if a[nx][ny] == '.' and c[nx][ny] == 0:
+                        c[nx][ny] = c[x][y] + 1
+                        q.append([nx, ny])
+                    elif a[nx][ny] == 'D':
+                        print(c[x][y])
+                        return
+            qlen -= 1
         water()
 
-    return False
+    print("KAKTUS")
+    return
 
 
-R, C = map(int, input().split())
-lst = []
-B, wat = [], deque()
-e = []
-for i in range(R):
-    lst.append(list(map(str, input().rstrip())))
-    for j in lst[i]:
-        if j == "D":
-            B = [i, lst[i].index("D")]
-        elif j == "*":
-            wat.append([i, lst[i].index("*")])
-        elif j == "S":
-            e = [i, lst[i].index("S")]
+def water():
+    qlen = len(wq)
+    while qlen:
+        x, y = wq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if a[nx][ny] == '.':
+                    a[nx][ny] = '*'
+                    wq.append([nx, ny])
+        qlen -= 1
 
-visit = [[-1 for _ in range(C)] for _ in range(R)]
-direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+n, m = map(int, input().split())
+a = [list(map(str, input())) for _ in range(n)]
+c = [[0] * m for _ in range(n)]
+q, wq = deque(), deque()
+
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 'S':
+            x1, y1 = i, j
+            a[i][j] = '.'
+        elif a[i][j] == '*':
+            wq.append([i, j])
 
 water()
-res = bfs(B[0], B[1])
-
-if res:
-    print(visit[e[0]][e[1]])
-else:
-    print("KAKTUS")
+bfs(x1, y1)

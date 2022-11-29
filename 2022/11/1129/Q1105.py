c1660cca135b14036893d280fb569fef99fc3914
701tkdgns <701tkdgns@inu.ac.kr>
2022-11-29 22:11:16 +0900

    1129

---
+++ 2022/11/1129/Q1105.py
@@ -0,0 +1,31 @@
+# L, R = map(str, input().split())
+# cnt = 0
+# if len(L) != len(R):
+#     print(0)
+# else:
+#     for i in range(len(L)):
+#         if L[i] == R[i] and L[i] == "8":
+#             cnt += 1
+#         else:
+#             break
+#     print(cnt)
+
+import sys
+
+read = lambda: sys.stdin.readline().rstrip()
+
+L, R = read().split()
+
+answer = 0
+
+if len(L) != len(R):
+    print(0)
+else:
+    for i in range(len(L)):
+        if L[i] == R[i]:
+            if L[i] == '8':
+                answer += 1
+        else:
+            break
+
+    print(answer)

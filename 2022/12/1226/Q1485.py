fc17ad2d06ef8b570854848ae75c1f3fecd79881
701tkdgns <701tkdgns@inu.ac.kr>
2022-12-27 23:41:04 +0900

    1227

--- 2022/12/1226/Q1485.py
+++ 2022/12/1226/Q1485.py
@@ -1,12 +1,18 @@
+# 각 4변의 길이가 모두 같고, 양 점에서 그은 두 대각선의 길이가 같다면 정사각형이다.
+
 for _ in range(int(input())):
     lst, x, y = [0 for _ in range(6)], [], []
-    a, b = map(int, input().split())
-    x.append(a)
-    y.append(b)
+    for _ in range(4):
+        a, b = map(int, input().split())
+        x.append(a)
+        y.append(b)
     k = 0
     for i in range(4):
         for j in range(i + 1, 4):
             lst[k] = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j])
             k += 1
     lst.sort()
-    print(lst)
+    if lst[0] == lst[1] == lst[2] == lst[3] and lst[4] == lst[5]:
+        print(1)
+    else:
+        print(0)

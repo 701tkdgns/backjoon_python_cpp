ace454e03f5625f89f112cad3513e57cbcd4bf96
701tkdgns <701tkdgns@inu.ac.kr>
2023-01-16 21:59:18 +0900

    0116

---
+++ 2023/1/0116/Q16236.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+def bfs(init_x, init_y, s):
+    dq = deque()
+    dq.append([init_x, init_y])
+    cand = []
+    while dq:
+        x, y = dq.popleft()
+        for dx, dy in direction:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N and not visit[nx][ny]:
+                pass
+    return
+
+
+N = int(input())
+lst = []
+visit = [[False for _ in range(N)] for _ in range(N)]
+direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+start_x, start_y = 0, 0
+shark = 1
+for i in range(N):
+    tmp = list(map(int, input().split()))
+    for j in range(N):
+        if tmp[j] == 9:
+            start_x, start_y = i, j
+            tmp[j] = 0
+    lst.append(tmp)
+
+bfs(start_x, start_y, shark)

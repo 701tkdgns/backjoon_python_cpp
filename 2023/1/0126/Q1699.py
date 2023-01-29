e38c8558d23f5cecf5a588bc627b94311546b0fc
701tkdgns <701tkdgns@inu.ac.kr>
2023-01-29 10:36:43 +0900

    0129

--- 2023/1/0126/Q1699.py
+++ 2023/1/0126/Q1699.py
@@ -2,9 +2,10 @@ N = int(input())
 dp = [0 for _ in range(N + 1)]
 for i in range(N + 1):
     dp[i] = i
-for i in range(N):
-    for j in range(1, i):
-        if j ** 2 <= i:
-            dp[i] = min(dp[i], dp[i - (j * j)] + 1)
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        if j ** 2 > i:
+            break
+        dp[i] = min(dp[i], dp[i - (j * j)] + 1)
 
 print(dp[N])

610e40a02ceb190ff6049d4d3e79163e582481e3
701tkdgns <701tkdgns@inu.ac.kr>
2023-02-02 00:43:26 +0900

    0202

--- 2023/2/Q9465.py
+++ 2023/2/Q9465.py
@@ -2,8 +2,15 @@ T = int(input())
 for _ in range(T):
     N = int(input())
     lst = []
-    dp = [0 for _ in range(N + 1)]
-    visit = [0 for _ in range(N + 1)]
+    dp = [[0 for _ in range(N + 1)] for _ in range(2)]
     for _ in range(2):
         lst.append(list(map(int, input().split())))
-    for i in range(N):
+    dp[0] = lst[0]
+    for i in range(1, N):
+        if i == 1:
+            dp[0][i] += lst[1][i - 1]
+            dp[1][i] += lst[0][i - 1]
+        else:
+            dp[0][i] = max(lst[0][i] + dp[1][i - 1], lst[0][i] + dp[1][i - 2])
+            dp[1][i] = max(lst[1][i] + dp[0][i - 1], lst[1][i] + dp[0][i - 2])
+    print(max(dp[0][N - 1], dp[1][N - 1]))

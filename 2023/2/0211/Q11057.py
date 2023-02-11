b3e68f994c77b544c1c0f34b1f1100c81b513e8d
psh <701tkdgns@inu.ac.kr>
2023-02-11 22:58:18 +0900

    0211

--- 2023/2/0211/Q11057.py
+++ 2023/2/0211/Q11057.py
@@ -1,6 +1,15 @@
 n = int(input())
 con = 10007
-dp = [0 for _ in range(10 ** n + 1)]
-dp[0] = 1
-for i in range(1, 10**n + 1):
-    pass
+dp = [[0 for _ in range(10)] for _ in range(1001)]
+for i in range(10):
+    dp[1][i] = 1
+for i in range(2, n + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = 1
+            continue
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % con
+res = 0
+for i in range(10):
+    res += dp[n][i]
+print(res)

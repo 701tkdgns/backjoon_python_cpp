7ef841f28821133ff38a1884432d8941f35cdc86
701tkdgns <701tkdgns@inu.ac.kr>
2023-02-16 22:48:43 +0900

    0216

--- 2023/2/0211/Q11057.py
+++ 2023/2/0211/Q11057.py
@@ -2,5 +2,17 @@ n = int(input())
 con = 10007
 dp = [0 for _ in range(10 ** n + 1)]
 dp[0] = 1
-for i in range(1, 10**n + 1):
-    pass
+for i in range(1, 10 ** n):
+    v = str(i)
+
+    if len(v) == 1:
+        dp[i] = 1
+        continue
+    chk = True
+    for j in range(1, len(v)):
+        if v[j] < v[j - 1]:
+            chk = False
+            break
+    if chk:
+        dp[i] = 1
+print(sum(dp) % con)

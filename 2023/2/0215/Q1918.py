186b7a6348752df2528925e43b19a34bf2b9b556
701tkdgns <701tkdgns@inu.ac.kr>
2023-02-16 23:27:16 +0900

    0216

--- 2023/2/0215/Q1918.py
+++ 2023/2/0215/Q1918.py
@@ -1,1 +1,23 @@
-s = input()
+s = list(input())
+stk = []
+
+for i in range(len(s)):
+    if s[i] == "(":
+        stk.append(s[i])
+    if s[i] in "+-":
+        while stk and stk[-1] != "(":
+            print(stk.pop(), end="")
+        stk.append(s[i])
+    if s[i] in "/*":
+        while stk and stk[-1] != "(" and (stk[-1] == '*' or stk[-1] == "/"):
+            print(stk.pop(), end="")
+        stk.append(s[i])
+    if s[i] == ")":
+        while stk and stk[-1] != "(":
+            print(stk.pop(), end="")
+        stk.pop()
+    if 'A' <= s[i] <= 'Z':
+        print(s[i], end="")
+
+while stk:
+    print(stk.pop(), end="")

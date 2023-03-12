2e796cef7a967134c6fe0aee712a692d09f90547
701tkdgns <701tkdgns@inu.ac.kr>
2023-03-12 13:24:16 +0900

    0312

--- 2023/3/0311/Q1377.py
+++ 2023/3/0311/Q1377.py
@@ -1,17 +1,13 @@
 import sys
-input = sys.stdin.readline
-N = int(input())
-lst = [0 for i in range(N)]
-ch = False
-for i in range(N):
-    lst[i] = int(input())
 
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
 for i in range(N):
-    ch = False
-    for j in range(N - i - 1):
-        if lst[j] > lst[j + 1]:
-            ch = True
-            lst[j], lst[j + 1] = lst[j + 1], lst[j]
-    if not ch:
-        print(i + 1)
-        break
+    lst.append([int(input()), i])
+_lst = sorted(lst)
+res = -1
+for i in range(N):
+    res = max(res, _lst[i][1] - lst[i][1])
+print(res + 1)

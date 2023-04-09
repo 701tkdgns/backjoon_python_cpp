968e14c8156c4ba2523569b6cf46a5ba46235197
psh <701tkdgns@inu.ac.kr>
2023-04-09 22:12:58 +0900

    0409

--- 2023/4/0408/Q2096.py
+++ 2023/4/0408/Q2096.py
@@ -1,22 +1,25 @@
 N = int(input())
-lst = []
-mx = [[0 for _ in range(N)] for _ in range(N)]
-mn = [[10e9 for _ in range(N)] for _ in range(N)]
-direction = [[-1, 1], [0, 1], [1, 1]]
-for _ in range(N):
-    lst.append(list(map(int, input().split())))
-for i in range(N):
-    mx[0][i] = lst[0][i]
-    mn[0][i] = lst[0][i]
+
+max_dp = [0] * 3
+min_dp = [0] * 3
+
+max_tmp = [0] * 3
+min_tmp = [0] * 3
 
 for i in range(N):
-    if i == N - 1:
-        break
-    for j in range(N):
-        for dx, dy in direction:
-            nx, ny = i + dx, j + dy
-            if 0 <= nx < N and 0 <= ny < N:
-                mx[nx][ny] = max(mx[nx][ny], lst[i][j] + lst[nx][ny])
-                mn[nx][ny] = min(mn[nx][ny], lst[i][j] + lst[nx][ny])
-print(mn)
-print(mx)
+    a, b, c = map(int, input().split())
+    for j in range(3):
+        if j == 0:
+            max_tmp[j] = a + max(max_dp[j], max_dp[j + 1])
+            min_tmp[j] = a + min(min_dp[j], min_dp[j + 1])
+        elif j == 1:
+            max_tmp[j] = b + max(max_dp[j - 1], max_dp[j], max_dp[j + 1])
+            min_tmp[j] = b + min(min_dp[j - 1], min_dp[j], min_dp[j + 1])
+        else:
+            max_tmp[j] = c + max(max_dp[j], max_dp[j - 1])
+            min_tmp[j] = c + min(min_dp[j], min_dp[j - 1])
+    for j in range(3):
+        max_dp[j] = max_tmp[j]
+        min_dp[j] = min_tmp[j]
+
+print(max(max_dp), min(min_dp))

9228deb617ca919eabebe06c17d863c2c7a97b41
psh <701tkdgns@inu.ac.kr>
2023-05-03 23:20:12 +0900

    0503

---
+++ 2023/4/0423/Q1786.py
@@ -0,0 +1,46 @@
+# def make_table():
+#     tmp = [0 for _ in range(len(P))]
+#     j = 0
+#     for i in range(1, len(P)):
+#         while j > 0 and P[i] != P[j]:
+#             j = tmp[j - 1]
+#         if P[i] == P[j]:
+#             j += 1
+#             tmp[i] = j
+#     return tmp
+#
+#
+# def KMP():
+#     cnt = 0
+#     pos = []
+#     j = 0
+#     for i in range(len(T)):
+#         while j > 0 and T[i] != P[j]:
+#             j = table[j - 1]
+#         if T[i] == P[j]:
+#             if j == len(P) - 1:
+#                 cnt += 1
+#                 pos.append(i - len(P) + 2)
+#             else:
+#                 j += 1
+#     return cnt, pos
+#
+#
+# T = input()
+# P = input()
+# table = make_table()
+# ans, position = KMP()
+# print(ans)
+# print(position)
+
+def make_table():
+    pass
+
+def KMP():
+    pass
+
+T = input()
+P = input()
+table = make_table()
+ans, position = KMP()
+print(position)
--- 2023/5/0501/Q1725.py
+++ 2023/5/0501/Q1725.py
@@ -1,3 +1,16 @@
+# n = int(input())
+# lst = [int(input()) for _ in range(n)]
+# lst.append(0)
+# res = -1
+# stk = [[0, lst[0]]]
+# for i in range(1, n + 1):
+#     left = i
+#     while stk and stk[-1][1] > lst[i]:
+#         left, h = stk.pop()
+#         res = max(res, (i - left) * h)
+#     stk.append([left, lst[i]])
+# print(res)
+
 n = int(input())
 lst = [int(input()) for _ in range(n)]
 lst.append(0)

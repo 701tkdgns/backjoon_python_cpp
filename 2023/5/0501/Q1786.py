27ee373977f53245b20ac08a7966ed1e36524b6c
701tkdgns <701tkdgns@inu.ac.kr>
2023-05-07 10:20:44 +0900

    0507

--- 2023/5/0501/Q1786.py
+++ 2023/5/0501/Q1786.py
@@ -1,39 +1,78 @@
-# https://bowbowbow.tistory.com/6
-# https://hooongs.tistory.com/305
+# # # https://bowbowbow.tistory.com/6
+# # # https://hooongs.tistory.com/305
+# #
+# #
+# # def make_table(_p):
+# #     tmp = [0] * len(_p)
+# #     j = 0
+# #     for i in range(1, len(_p)):
+# #         while j > 0 and _p[i] != _p[j]:
+# #             j = tmp[j - 1]
+# #         if _p[i] == _p[j]:
+# #             j += 1
+# #             tmp[i] = j
+# #     return tmp
+# #
+# #
+# # def kmp(_s, _p):
+# #     cnt = 0
+# #     pos = []
+# #     j = 0
+# #     for i in range(len(_s)):
+# #         while j > 0 and _s[i] != _p[j]:
+# #             j = table[j - 1]
+# #         if _s[i] == _p[j]:
+# #             if j == len(p) - 1:
+# #                 cnt += 1
+# #                 pos.append(i - len(p) + 2)
+# #                 j = table[j]
+# #             else:
+# #                 j += 1
+# #     return cnt, pos
+# #
+# #
+# # s = input()
+# # p = input()
+# # table = make_table(p)
+# # res, positions = kmp(s, p)
+# # print(res)
+# # print(*positions)
+#
+# def makeTable(p):
+#     tmp = [0 for _ in range(len(p))]
+#     j = 0
+#     for i in range(1, len(p)):
+#         while j > 0 and p[i] != p[j]:
+#             j = tmp[j - 1]
+#         if p[i] == p[j]:
+#             j += 1
+#             tmp[i] = j
+#
+#     return tmp
+#
+# def kmp(s, p):
+#     pass
+#
+#
+# s = input()
+# p = input()
+# table = makeTable(p)
+# print(table)
+# # res, positions = kmp(s, p)
+# # print(res)
+# # print(*positions)
 
 
-def make_table(_p):
-    tmp = [0] * len(_p)
-    j = 0
-    for i in range(1, len(_p)):
-        while j > 0 and _p[i] != _p[j]:
-            j = tmp[j - 1]
-        if _p[i] == _p[j]:
-            j += 1
-            tmp[i] = j
-    return tmp
 
 
-def kmp(_s, _p):
-    cnt = 0
-    pos = []
-    j = 0
-    for i in range(len(_s)):
-        while j > 0 and _s[i] != _p[j]:
-            j = table[j - 1]
-        if _s[i] == _p[j]:
-            if j == len(p) - 1:
-                cnt += 1
-                pos.append(i - len(p) + 2)
-                j = table[j]
-            else:
-                j += 1
-    return cnt, pos
 
 
-s = input()
-p = input()
-table = make_table(p)
-res, positions = kmp(s, p)
-print(res)
-print(*positions)
+
+
+
+
+
+
+
+
+
--- 2023/5/0501/Q2357.py
+++ 2023/5/0501/Q2357.py
@@ -1,38 +1,91 @@
-# https://squareyun.tistory.com/103
+# # https://squareyun.tistory.com/103
+#
+# import math
+# import sys
+# sys.setrecursionlimit(10**6)
+#
+# def makeSegTree(idx, s, e):
+#     if s == e:
+#         seg[idx] = [lst[s], lst[s]]
+#         return seg[idx]
+#     m = (s + e) // 2
+#     left = makeSegTree(idx * 2, s, m)
+#     right = makeSegTree(idx * 2 + 1, m + 1, e)
+#     seg[idx] = (min(left[0], right[0]), max(left[1], right[1]))
+#     return seg[idx]
+#
+#
+# def find(idx, s, e, l, r):
+#     if r < s or l > e:
+#         return [10e10 + 1, -1]
+#     if l <= s and e <= r:
+#         return seg[idx]
+#     m = (s + e) // 2
+#     left = find(idx * 2, s, m, l, r)
+#     right = find(idx * 2 + 1, m + 1, e, l, r)
+#     return min(left[0], right[0]), max(left[1], right[1])
+#
+#
+# n, m = map(int, input().split())
+# lst = [int(input()) for _ in range(n)]
+# h = math.ceil(math.log2(n)) + 1
+# nodeNum = 1 << h
+# seg = [0] * nodeNum
+# makeSegTree(1, 0, len(lst) - 1)
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     res = find(1, 0, len(lst) - 1, a - 1, b - 1)
+#     print(*res)
+
 
 import math
 import sys
-sys.setrecursionlimit(10**6)
 
-def makeSegTree(idx, s, e):
-    if s == e:
-        seg[idx] = [lst[s], lst[s]]
+sys.setrecursionlimit(10 ** 8)  # 재귀 깊이 제한 늘리기
+input = sys.stdin.readline
+
+
+def makeSegTree(idx, start, end):
+    if start == end:  # 리프 노드
+        seg[idx] = (arr[start], arr[start])  # 최소값, 최대값은 동일
         return seg[idx]
-    m = (s + e) // 2
-    left = makeSegTree(idx * 2, s, m)
-    right = makeSegTree(idx * 2 + 1, m + 1, e)
+
+    mid = (start + end) // 2  # 몫 구하기
+
+    left = makeSegTree(idx * 2, start, mid)
+    right = makeSegTree(idx * 2 + 1, mid + 1, end)
+
+    # 왼쪽과 오른쪽 중 각각 min, max 계산
     seg[idx] = (min(left[0], right[0]), max(left[1], right[1]))
+
     return seg[idx]
 
 
-def find(idx, s, e, l, r):
-    if r < s or l > e:
-        return [10e10 + 1, -1]
-    if l <= s and e <= r:
+def find(idx, start, end):
+    # 찾으려는 범위가 전혀 겹치지 않을 때
+    if range2 < start or range1 > end:
+        return (10 ** 9 + 1, 0)  # 최대 min 값, 최소 max 값 반환
+
+    # 찾으려는 범위가 트리 범위에 속할 때
+    if range1 <= start and end <= range2:
         return seg[idx]
-    m = (s + e) // 2
-    left = find(idx * 2, s, m, l, r)
-    right = find(idx * 2 + 1, m + 1, e, l, r)
-    return min(left[0], right[0]), max(left[1], right[1])
+
+    mid = (start + end) // 2
+    left = find(idx * 2, start, mid)
+    right = find(idx * 2 + 1, mid + 1, end)
+    return (min(left[0], right[0]), max(left[1], right[1]))
 
 
 n, m = map(int, input().split())
-lst = [int(input()) for _ in range(n)]
-h = math.ceil(math.log2(n)) + 1
-nodeNum = 1 << h
-seg = [0] * nodeNum
-makeSegTree(1, 0, len(lst) - 1)
+arr = [int(input()) for _ in range(n)]  # 이런 방식으로 입력받을 수 있다니
+
+h = math.ceil(math.log2(n)) + 1  # 노드 개수에 따른 트리의 높이 계산 공식
+nodeNum = 1 << h  ## 2^h 계산
+seg = [0 for _ in range(nodeNum)]
+makeSegTree(1, 0, len(arr) - 1)
+
 for _ in range(m):
-    a, b = map(int, input().split())
-    res = find(1, 0, len(lst) - 1, a - 1, b - 1)
-    print(*res)
+    range1, range2 = map(int, input().split())
+    range1, range2 = range1 - 1, range2 - 1  # index이므로 -1 해주기
+    answer = find(1, 0, len(arr) - 1)
+    print(answer[0], answer[1])

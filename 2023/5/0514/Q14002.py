ef45635ac012fb34633ad5e37732cb1f29c2e487
701tkdgns <701tkdgns@inu.ac.kr>
2023-05-20 10:54:47 +0900

    0520

--- 2023/5/0514/Q14002.py
+++ 2023/5/0514/Q14002.py
@@ -1,3 +1,17 @@
 N = int(input())
 A = list(map(int, input().split()))
-dp = [0 for _ in range(N + 1)]
+dp = [1 for _ in range(N)]
+for i in range(N):
+    cnt = 0
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
+sub = []
+order = max(dp)
+for i in range(N - 1, -1, -1):
+    if dp[i] == order:
+        sub.append(A[i])
+        order -= 1
+sub.reverse()
+print(*sub)

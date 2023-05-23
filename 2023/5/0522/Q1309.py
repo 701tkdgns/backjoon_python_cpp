dd13f5ec01055b4f58c4c1fff8ac6fda8d270ad3
psh <701tkdgns@inu.ac.kr>
2023-05-23 23:53:25 +0900

    0523

--- 2023/5/0522/Q1309.py
+++ 2023/5/0522/Q1309.py
@@ -1,7 +1,18 @@
-n = int(input())
-# dp = [[0 for _ in range(2)] for _ in range(n + 1)]
-dp = [1, 3] + [0] * (n - 1)
-# print(dp)
-# dp[0][0], dp[1][0] = 1, 1
-for i in range(2, n):
-    dp[i] = dp[i - 2] + dp[i - 1] * 2
+# N = int(input())
+# dp = [0 for _ in range(N + 1)]
+# dp[0], dp[1] = 1, 3
+# for i in range(2, N + 1):
+#     dp[i] = dp[i - 2] + dp[i - 1] * 2
+# print(dp[N])
+#
+
+N = int(input())
+mod = 9901
+dp = [[0 for _ in range(3)] for _ in range(N + 1)]
+dp[1][0], dp[1][1], dp[1][2] = 1, 1, 1
+for i in range(2, N + 1):
+    dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % mod
+    dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod
+    dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % mod
+res = (dp[N][0] + dp[N][1] + dp[N][2]) % mod
+print(res)

8f60721e5a89cd92aaa22fb01411c841ed5a5e18
psh <701tkdgns@inu.ac.kr>
2023-05-30 22:51:08 +0900

    0530

---
+++ 2023/5/0530/Q2252.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+N, M = map(int, input().split())
+lst = [[] for _ in range(N + 1)]
+visit = [False for _ in range(N + 1)]
+inDegree = [0 for _ in range(N + 1)]
+dq = deque()
+res = []
+for _ in range(M):
+    a, b = map(int, input().split())
+    lst[a].append(b)
+    inDegree[b] += 1
+for i in range(1, N + 1):
+    if inDegree[i] == 0:
+        dq.append(i)
+
+while dq:
+    v = dq.popleft()
+    res.append(v)
+    for i in lst[v]:
+        inDegree[i] -= 1
+        if inDegree[i] == 0:
+            dq.append(i)
+print(*res)

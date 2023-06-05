12324924a29c765c1ada8f5c930ad7d814bc2128
701tkdgns <701tkdgns@inu.ac.kr>
2023-06-05 11:43:28 +0900

    0605

--- 2023/6/0604/Q9252.py
+++ 2023/6/0604/Q9252.py
@@ -1,14 +1,20 @@
-f = input()
-s = input()
-dp = [['' for _ in range(len(f))] for _ in range(len(s))]
-for i in range(len(f)):
-    for j in range(len(s)):
-        if f[i] == s[j]:
-            dp[i][j] = dp[i - 1][j - 1] + f[i]
+import sys
+
+input = sys.stdin.readline
+F = [""] + list(map(str, input().rstrip()))
+S = [""] + list(map(str, input().rstrip()))
+dp = [["" for _ in range(len(S))] for _ in range(len(F))]
+
+for i in range(1, len(F)):
+    for j in range(1, len(S)):
+        if F[i] == S[j]:
+            dp[i][j] = dp[i - 1][j - 1] + F[i]
         else:
             if len(dp[i - 1][j]) >= len(dp[i][j - 1]):
                 dp[i][j] = dp[i - 1][j]
             else:
                 dp[i][j] = dp[i][j - 1]
-print(len(dp[len(f) - 1][len(s) - 1]))
-print(dp[len(f) - 1][len(s) - 1])
+f, s = len(F), len(S)
+
+print(len(dp[f - 1][s - 1]))
+print(dp[f - 1][s - 1])

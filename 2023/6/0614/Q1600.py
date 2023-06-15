41d6ca13d6dd4659afb67ea01de2847c9a41231d
psh <701tkdgns@inu.ac.kr>
2023-06-15 23:54:38 +0900

    0615

--- 2023/6/0614/Q1600.py
+++ 2023/6/0614/Q1600.py
@@ -2,24 +2,34 @@ from collections import deque
 
 
 def bfs():
-    visit[0][0][0] = 0
+    visit[0][0][0] = 1
     dq = deque([[0, 0, 0]])
     while dq:
         x, y, z = dq.popleft()
+        if x == n - 1 and y == m - 1:
+            return visit[x][y][z] - 1
+
+        for dx, dy in direction:
+            nx, ny = dx + x, dy + y
+            if (0 <= nx < n and 0 <= ny < m) and visit[nx][ny][z] == 0 and lst[nx][ny] == 0:
+                visit[nx][ny][z] = visit[x][y][z] + 1
+                dq.append([nx, ny, z])
+
         if z < K:
             for dx, dy in horse_d:
                 hx, hy = dx + x, dy + y
-                print(hx, hy, z, "hello")
-                if (0 <= hx < H and 0 <= hy < W) and visit[hx][hy][z] == -1:
-                    print("hellox")
+                if (0 <= hx < n and 0 <= hy < m) and visit[hx][hy][z + 1] == 0 and lst[hx][hy] == 0:
+                    visit[hx][hy][z + 1] = visit[x][y][z] + 1
+                    dq.append([hx, hy, z + 1])
+    return -1
 
 
 K = int(input())
-W, H = map(int, input().split())
+m, n = map(int, input().split())
 lst = []
-visit = [[[-1 for _ in range(W)] for _ in range(H)] for _ in range(K + 1)]
+visit = [[[0] * (K + 1) for _ in range(m)] for _ in range(n)]
 direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 horse_d = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
-for _ in range(H):
+for _ in range(n):
     lst.append(list(map(int, input().split())))
-bfs()
+print(bfs())

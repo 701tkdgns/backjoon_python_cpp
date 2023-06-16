0b4ff97a92d415c924163f6bb3d5e380d0e8a4cb
psh <701tkdgns@inu.ac.kr>
2023-06-16 23:28:14 +0900

    0616

--- 2023/6/0615/linkedList.py
+++ 2023/6/0615/linkedList.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = next
 
+
 class linkedList:
     def __init__(self, data):
         self.head = Node(data)
@@ -21,3 +22,30 @@ class linkedList:
         while node:
             print(node.data)
             node = node.next
+
+    def delete(self, data):
+        if self.head == '':
+            return
+
+        if self.head.data == data:
+            tmp = self.head
+            self.head = self.head.next
+            del tmp
+
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    tmp = node.next
+                    node.next = node.next.next
+                    del tmp
+                else:
+                    node = node.next
+
+    def search(self, data):
+        node = self.head
+        while node:
+            if node.data == data:
+                return node
+            else:
+                node = node.next

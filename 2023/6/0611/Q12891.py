1ced3fd20cb8a94451d2846fe6b6c92bfe44cf94
701tkdgns <701tkdgns@inu.ac.kr>
2023-06-17 11:44:49 +0900

    0617

--- 2023/6/0611/Q12891.py
+++ 2023/6/0611/Q12891.py
@@ -1,17 +1,24 @@
-s, p = map(int, input().split())
-string = input()
-a, c, g, t = map(int, input().split())
-dna = {"A": 0, "C": 0, "G": 0, "T": 0}
-res, cnt = 0, 0
-start = string[:p]
-for i in start:
-    dna[i] += 1
-if dna["A"] >= a and dna["C"] >= c and dna["G"] >= g and dna["T"] >= t:
-    cnt += 1
-start_idx, end_idx = 0, p
-for i in range(len(string) - p):
-    dna[string[start_idx + i]] -= 1
-    dna[string[end_idx + i]] += 1
+def check():
     if dna["A"] >= a and dna["C"] >= c and dna["G"] >= g and dna["T"] >= t:
+        return True
+    else:
+        return False
+
+
+S, P = map(int, input().split())
+string = input()
+cnt = 0
+dna = {"A": 0, "C": 0, "G": 0, "T": 0}
+a, c, g, t = map(int, input().split())
+s, e = 0, P
+chk = string[:P]
+for i in range(len(chk)):
+    dna[chk[i]] += 1
+if check():
+    cnt += 1
+for i in range(S - P):
+    dna[string[s + i]] -= 1
+    dna[string[e + i]] += 1
+    if check():
         cnt += 1
 print(cnt)

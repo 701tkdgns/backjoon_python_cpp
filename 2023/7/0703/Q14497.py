c7e1038adcfbf6235e7411201d37be48dd47d407
psh <701tkdgns@inu.ac.kr>
2023-07-05 23:00:06 +0900

    0705

--- 2023/7/0703/Q14497.py
+++ 2023/7/0703/Q14497.py
@@ -1,4 +1,4 @@
-import heapq
+from collections import deque
 
 direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
@@ -6,21 +6,18 @@ N, M = map(int, input().split())
 x1, y1, x2, y2 = map(int, input().split())
 lst = [list(map(str, input().rstrip())) for _ in range(N)]
 visit = [[-1 for _ in range(M)] for _ in range(N)]
-hq = []
-heapq.heappush(hq, [x1, y1])
-visit[x1][y1] = 0
-while hq:
-    x, y = heapq.heappop(hq)
-    if x == x2 and y == y2:
-        break
+dq = deque([])
+visit[x1 - 1][y1 - 1] = 0
+dq.append([x1 - 1, y1 - 1])
+while dq:
+    x, y = dq.popleft()
     for dx, dy in direction:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < M and visit[nx][ny] == -1:
             if lst[nx][ny] == "1" or lst[nx][ny] == "#":
                 visit[nx][ny] = visit[x][y] + 1
-                heapq.heappush(hq, [nx, ny])
-                # break
-            if lst[nx][ny] == "0":
-                visit[nx][ny] = visit[x][y] + 1
-                heapq.heappush(hq, [nx, ny])
-print(visit[x2][y2])
+                dq.append([nx, ny])
+            elif lst[nx][ny] == "0":
+                visit[nx][ny] = visit[x][y]
+                dq.appendleft([nx, ny])
+print(visit[x2 - 1][y2 - 1])

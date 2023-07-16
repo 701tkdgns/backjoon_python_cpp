0e5bcf60416d6f2b7b83945d8794779b516496f5
701tkdgns <701tkdgns@inu.ac.kr>
2023-07-16 13:01:37 +0900

    0716

--- 2023/7/0709/Q11559.py
+++ 2023/7/0709/Q11559.py
@@ -1,41 +1,55 @@
 from collections import deque
 
 
-def check(x, y):
+def delete(_tmp):
+    for x, y in _tmp:
+        lst[x][y] = "."
+
+
+def down():
+    for i in range(6):
+        for j in range(10, -1, -1):
+            for k in range(11, j, -1):
+                if lst[j][i] != "." and lst[k][i] == ".":
+                    lst[k][i] = lst[j][i]
+                    lst[j][i] = "."
+                    break
+
+
+def bfs(x, y):
     dq = deque([[x, y]])
     now = lst[x][y]
-    pos = []
+    visit[x][y] = 1
+    tmp.append([x, y])
+
     while dq:
         x, y = dq.popleft()
         for dx, dy in direction:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < 12 and 0 <= ny < 6 and lst[nx][ny] == now and visit[nx][ny] == 0:
+            if 0 <= nx < 12 and 0 <= ny < 6 and lst[nx][ny] == now and not visit[nx][ny]:
                 visit[nx][ny] = 1
-                pos.append([x, y])
                 dq.append([nx, ny])
-
-    pos.sort(key=lambda x: (x[1], x[0]))
-    for i, j in pos:
-        lst[i][j] = "_"
-        bombList.append([i, j])
+                tmp.append([nx, ny])
 
 
 lst = [list(map(str, input().rstrip())) for _ in range(12)]
 direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 res = 0
+
 while True:
     visit = [[0 for _ in range(6)] for _ in range(12)]
-    bombList = []
+    chk = 0
+
     for i in range(12):
         for j in range(6):
-            if lst[i][j] != "." and lst[i][j] != "_" and visit[i][j] == 0:
-                check(i, j)
-    if len(bombList) == 0:
+            if lst[i][j] != "." and not visit[i][j]:
+                tmp = []
+                bfs(i, j)
+                if len(tmp) >= 4:
+                    chk = 1
+                    delete(tmp)
+    if not chk:
         break
-
-    for bomb in bombList:
-        x, y = bomb[0], bomb[1]
-        for i in range(x, 0, -1):
-            lst[i][y] = lst[i - 1][y]
-        lst[0][y] = "."
+    down()
     res += 1
+print(res)

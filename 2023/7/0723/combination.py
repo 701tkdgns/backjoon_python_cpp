620e21206985229418289114e96e04ede5f97c9b
psh <701tkdgns@inu.ac.kr>
2023-08-03 21:23:19 +0900

    0803

--- 2023/7/0723/combination.py
+++ 2023/7/0723/combination.py
@@ -14,21 +14,21 @@
 #     generate([])
 # combination('ABCDE', 2)
 
-def combination(arr, r):
-    arr = sorted(arr)
-
-    def generate(chosen):
-        if len(chosen) == r:
-            print(chosen)
-            return
-        start = arr.index(chosen[-1]) + 1 if chosen else 0
-        for nxt in range(start, len(arr)):
-            chosen.append(arr[nxt])
-            generate(chosen)
-            chosen.pop()
-    generate([])
-
-combination("ABCDE", 2)
+# def combination(arr, r):
+#     arr = sorted(arr)
+#
+#     def generate(chosen):
+#         if len(chosen) == r:
+#             print(chosen)
+#             return
+#         start = arr.index(chosen[-1]) + 1 if chosen else 0
+#         for nxt in range(start, len(arr)):
+#             chosen.append(arr[nxt])
+#             generate(chosen)
+#             chosen.pop()
+#     generate([])
+#
+# combination("ABCDE", 2)
 
 # def combination(arr, r):
 #     for i in range(len(arr)):
@@ -41,3 +41,41 @@ combination("ABCDE", 2)
 #
 # for a in combination('ABCDE', 2):
 #     print(a)
+
+def combination(arr, r):
+    _arr = sorted(arr)
+
+    def generate(chosen):
+        if len(chosen) == r:
+            print(chosen)
+            return
+        start = _arr.index(chosen[-1]) + 1 if chosen else 0
+        for i in range(start, len(_arr)):
+            chosen.append(_arr[i])
+            generate(chosen)
+            chosen.pop()
+
+    generate([])
+
+
+def permutation(arr, r):
+    _arr = sorted(arr)
+    visit = [0] * len(arr)
+
+    def generate(chosen):
+        if len(chosen) == r:
+            print(chosen)
+            return
+        for i in range(len(_arr)):
+            if visit[i] == 0:
+                visit[i] = 1
+                chosen.append(_arr[i])
+                generate(chosen)
+                chosen.pop()
+                visit[i] = 0
+
+    generate([])
+
+
+permutation("ABCD", 2)
+# combination("ABCD", 2)

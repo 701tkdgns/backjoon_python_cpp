a3cb929cca02f68ad39b319a6011e1dbabaa191b
701tkdgns <701tkdgns@inu.ac.kr>
2024-04-16 17:28:07 +0900

    240416

--- 2024/4/16/Q12933.py
+++ 2024/4/16/Q12933.py
@@ -1,8 +1,34 @@
-S = input()
-cnt = 0
-stk = []
-while S != '':
-    duck = ''
-    duck_idx = []
-    for i in S:
-        if i == ''
+sound = list(input())
+ans = 0
+
+if sound[0] != "q" or sound[-1] != "k" or len(sound) % 5:
+    print(-1)
+    exit()
+
+
+def find_quack(start):
+    quack = "quack"
+    j = 0
+    global ans
+    new_ori = True
+    for i in range(start, len(sound)):
+        if sound[i] == quack[j]:
+            if sound[i] == "k":
+                if new_ori:
+                    ans += 1
+                    new_ori = False
+                j = 0
+                sound[i] = 0
+                continue
+            j += 1
+            sound[i] = 0
+
+
+for i in range(len(sound) - 4):
+    if sound[i] == "q":
+        find_quack(i)
+
+if any(sound) or ans == 0:
+    print(-1)
+else:
+    print(ans)

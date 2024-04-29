a6585bcd89e63a1d7d003d25932b5a8fac19c5c6
701tkdgns <55741859+701tkdgns@users.noreply.github.com>
2024-04-29 09:41:15 +0900

    240429

---
+++ 2024/4/19/Q16719.py
@@ -0,0 +1,39 @@
+def dfs(idx, arr):
+    if not arr:
+        return
+    mn = min(arr)
+    mn_idx = arr.index(mn)
+    res[idx + mn_idx] = mn
+    print(''.join(res))
+    dfs(idx + mn_idx + 1, arr[mn_idx + 1:])
+    dfs(idx, arr[:mn_idx])
+
+g
+S = list(input().rstrip())
+res = ['' for _ in range(len(S))]
+dfs(0, S)
+
+
+
+
+
+
+
+
+
+
+
+# def dfs(idx, arr):
+#     if not arr:
+#         return
+#     mn = min(arr)
+#     mn_idx = arr.index(mn)
+#     res[idx + mn_idx] = mn
+#     print(''.join(res))
+#     dfs(idx + mn_idx + 1, arr[mn_idx + 1:])
+#     dfs(idx, arr[:mn_idx])
+#
+#
+# S = list(input().rstrip())
+# res = ['' for _ in range(len(S))]
+# dfs(0, S)

7561adbd56f4968a1d4e9a25df0da28476c879ac
701tkdgns <701tkdgns@inu.ac.kr>
2024-06-16 23:32:34 +0900

    240616

--- 2024/6/15/Q4195.py
+++ 2024/6/15/Q4195.py
@@ -1,5 +1,28 @@
+def find(v):
+    if parent[v] != v:
+        parent[v] = find(parent[v])
+    return parent[v]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parent[y] = x
+        number[x] = number[x] + number[y]
+    print(number[x])
+
+
 N = int(input())
 for _ in range(N):
     F = int(input())
-    for _ in range(F):
+    parent, number = {}, {}
+    for i in range(F):
         a, b = input().split()
+        if a not in parent:
+            parent[a] = a
+            number[a] = 1
+        if b not in parent:
+            parent[b] = b
+            number[b] = 1
+        union(a, b)

2c95c25bb6a9b80a23807100b64e51b1e8432087
701tkdgns <701tkdgns@inu.ac.kr>
2024-06-17 21:55:21 +0900

    240617

--- 2024/6/15/Q4195.py
+++ 2024/6/15/Q4195.py
@@ -1,28 +1,26 @@
-def find(v):
-    if parent[v] != v:
-        parent[v] = find(parent[v])
-    return parent[v]
-
+def find(x):
+    if x != parent[x]:
+        parent[x] = find(parent[x])
+    return parent[x]
 
 def union(x, y):
     x = find(x)
     y = find(y)
     if x != y:
         parent[y] = x
-        number[x] = number[x] + number[y]
-    print(number[x])
-
+        count[x] += count[y]
+    print(count[x])
 
 N = int(input())
 for _ in range(N):
     F = int(input())
-    parent, number = {}, {}
-    for i in range(F):
+    parent, count = {}, {}
+    for _ in range(F):
         a, b = input().split()
         if a not in parent:
             parent[a] = a
-            number[a] = 1
+            count[a] = 1
         if b not in parent:
             parent[b] = b
-            number[b] = 1
+            count[b] = 1
         union(a, b)

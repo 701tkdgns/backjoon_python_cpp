54e36a65df574dbd3a920733618b04f992fd4f6f
701tkdgns <701tkdgns@inu.ac.kr>
2024-08-08 16:17:18 +0900

    240808

---
+++ 2024/8/8/Q25497.py
@@ -0,0 +1,27 @@
+# L은 R의 사전 기술, S은 K의 사전 기술
+
+N = int(input())
+skills = input()
+SK, LR = [], []
+res = 0
+for s in skills:
+    if 49 <= ord(s) <= 58:
+        res += 1
+    else:
+        if s == 'S':
+            SK.append(s)
+        if s == 'L':
+            LR.append(s)
+        if s == 'K':
+            if SK:
+                SK.pop()
+                res += 1
+            else:
+                break
+        if s == 'R':
+            if LR:
+                LR.pop()
+                res += 1
+            else:
+                break
+print(res)

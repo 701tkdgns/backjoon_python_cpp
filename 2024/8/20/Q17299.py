8d30838dceb5f2dbc9a467ea902525c47f72485d
701tkdgns <55741859+701tkdgns@users.noreply.github.com>
2024-08-20 12:12:06 +0900

    240820

--- 2024/8/20/Q17299.py
+++ 2024/8/20/Q17299.py
@@ -1,10 +1,15 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
 N = int(input())
 lst = list(map(int, input().split()))
 ans = [-1 for _ in range(N)]
-cnt = dict()
+frequency = Counter(lst)
 stk = []
-for i in range(N):
-    cnt[lst[i]] = cnt.get(lst[i], 0) + 1
-for i in range(N):
+for i in range(N - 1, -1, -1):
+    while stk and frequency[lst[i]] >= frequency[stk[-1]]:
+        stk.pop()
+    if stk:
+        ans[i] = stk[-1]
     stk.append(lst[i])
-print(cnt)
+print(*ans)
